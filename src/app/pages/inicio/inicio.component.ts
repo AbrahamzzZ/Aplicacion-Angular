e2923981bf297c03490a3b6f596bd19f630b1602
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-18 23:27:14 -0500

    Cambio de las rutas de los 5 modulos

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, RouterOutlet],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
