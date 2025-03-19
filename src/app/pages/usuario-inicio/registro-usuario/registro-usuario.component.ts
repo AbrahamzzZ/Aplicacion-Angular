fe804250c0b7fc01c12e952d1780aa3b69d7365f
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-18 23:23:25 -0500

    Se mojoro la estetica del formulario y se agrego nuevas validaciones

--- src/app/pages/usuario-inicio/registro-usuario/registro-usuario.component.ts
+++ src/app/pages/usuario-inicio/registro-usuario/registro-usuario.component.ts
@@ -4,20 +4,24 @@ import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { Validaciones } from '../../../../utility/validaciones';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [MatCardModule, MatInput, MatFormFieldModule, MatButton, ReactiveFormsModule],
+  imports: [MatCardModule, MatInput, MatFormFieldModule, MatButton, MatCheckboxModule, ReactiveFormsModule, RouterLink],
   templateUrl: './registro-usuario.component.html',
   styleUrl: './registro-usuario.component.scss'
 })
-export class UsuarioComponent {
+export class RegistroUsuarioComponent {
 private readonly formBuilder = inject(FormBuilder);
   formGroup = this.formBuilder.nonNullable.group({
-    nombreCompleto: ['', Validators.required],
-    clave: ['', Validators.required],
-    correoElectronico: ['', Validators.required, Validators.email]
+    nombreCompleto: ['', [Validators.required, Validaciones.soloLetras()]],
+    clave: ['', [Validators.required, Validaciones.formatoClave()]],
+    correoElectronico: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+    estado: [false]
   });
 
   get nombreCompletoField(): FormControl<string> {
