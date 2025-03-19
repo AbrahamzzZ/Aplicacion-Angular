5f70316f79e2f0067f1c79e1c652138e5d7fca6d
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-18 23:20:45 -0500

    Se mejoro la estetica del formulario y se agrego nuevas validaciones

--- src/app/pages/cliente-inicio/registro-cliente/registro-cliente.component.ts
+++ src/app/pages/cliente-inicio/registro-cliente/registro-cliente.component.ts
@@ -4,22 +4,26 @@ import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { Validaciones } from '../../../../utility/validaciones';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cliente',
   standalone: true,
-  imports: [ MatCardModule, MatInput, MatFormFieldModule, MatButton, ReactiveFormsModule],
+  imports: [ MatCardModule, MatInput, MatFormFieldModule, MatButton, MatCheckboxModule, ReactiveFormsModule, RouterLink],
   templateUrl: './registro-cliente.component.html',
   styleUrl: './registro-cliente.component.scss'
 })
-export class ClienteComponent {
+export class RegistroClienteComponent {
   private readonly formBuilder = inject(FormBuilder);
   formGroup = this.formBuilder.nonNullable.group({
-    nombres: ['', Validators.required],
-    apellidos: ['', Validators.required],
-    cedula: ['', Validators.required],
-    telefono: ['', Validators.required],
-    correoElectronico: ['', Validators.required, Validators.email]
+    nombres: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(30), Validaciones.soloLetras()]],
+    apellidos: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(30), Validaciones.soloLetras()]],
+    cedula: ['', [Validators.required, Validaciones.soloNumeros()]],
+    telefono: ['', [Validators.required, Validaciones.soloNumeros()]],
+    correoElectronico: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+    estado: [false]
   });
 
   get nombresField(): FormControl<string> {
