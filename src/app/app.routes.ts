98a195dd22db0ec03366c1d589abbdf307222ed9
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-18 23:27:56 -0500

    Se cambiaron las rutas para los formularios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,16 @@ import { UsuarioInicioComponent } from './pages/usuario-inicio/usuario-inicio.co
 import { ClienteInicioComponent } from './pages/cliente-inicio/cliente-inicio.component';
 import { PaginaNoEncontradaComponent } from './pages/pagina-no-encontrada/pagina-no-encontrada.component';
 import { TransportistaInicioComponent } from './pages/transportista-inicio/transportista-inicio.component';
-import { ProveedorInicioComponent } from './pages/proveedor-inicio/proveedor-inicio.component';
-import { ProductoInicioComponent } from './pages/producto-inicio/producto-inicio.component';
+import { RegistroProveedorInicioComponent } from './pages/proveedor-inicio/proveedor-inicio.component';
+import { RegistroProductoInicioComponent } from './pages/producto-inicio/producto-inicio.component';
 import { ProductoComponent } from './pages/producto-inicio/registro-producto/registro-producto.component';
-import { ClienteComponent } from './pages/cliente-inicio/registro-cliente/registro-cliente.component';
-import { UsuarioComponent } from './pages/usuario-inicio/registro-usuario/registro-usuario.component';
-import { TransportistaComponent } from './pages/transportista-inicio/registro-transportista/registro-transportista.component';
+import { RegistroClienteComponent } from './pages/cliente-inicio/registro-cliente/registro-cliente.component';
+import { RegistroUsuarioComponent } from './pages/usuario-inicio/registro-usuario/registro-usuario.component';
+import { RegistroTransportistaComponent } from './pages/transportista-inicio/registro-transportista/registro-transportista.component';
 import { ProveedorComponent } from './pages/proveedor-inicio/registro-proveedor/registro-proveedor.component';
 
 export const routes: Routes = [
-    {path: 'home', component: InicioComponent, title: 'Supermercado Paradisia'},
+    /*{path: 'home', component: InicioComponent, title: 'Supermercado Paradisia'},
     {path: 'usuario', component: UsuarioInicioComponent, title: 'Usuarios', children: [{
         path: 'usuario-detalle', component: UsuarioComponent, title: 'Información del Usuario'
     }]},
@@ -30,6 +30,18 @@ export const routes: Routes = [
         path: 'producto-detalle', component: ProductoComponent, title: 'Información del Producto'
     }]},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: '**', component: PaginaNoEncontradaComponent}
-
+    {path: '**', component: PaginaNoEncontradaComponent}*/
+    {path: 'home', component: InicioComponent, title: 'Supermercado Paradisia'},
+    {path: 'usuario', component: UsuarioInicioComponent, title: 'Usuarios'},
+    {path: 'usuario/usuario-registro', component: RegistroUsuarioComponent, title: 'Información del Usuario'},
+    {path: 'cliente', component: ClienteInicioComponent, title: 'Clientes'},
+    {path: 'cliente/cliente-registro', component: RegistroClienteComponent, title: 'Información del Cliente'},
+    {path: 'transportista', component: TransportistaInicioComponent, title: 'Transportistas'},
+    {path: 'transportista/transportista-registro', component: RegistroTransportistaComponent, title: 'Información del Transportista'},
+    {path: 'proveedor', component: RegistroProveedorInicioComponent, title: 'Proveedor'},
+    {path: 'proveedor/proveedor-registro', component: ProveedorComponent, title: 'Información del Proveedor'},
+    {path: 'producto', component: RegistroProductoInicioComponent, title: 'Productos'},
+    {path: 'producto/producto-registro', component: ProductoComponent, title: 'Información del Producto'},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: PaginaNoEncontradaComponent, title: 'Pagina no existente'}
 ];
