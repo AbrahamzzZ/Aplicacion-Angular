0c9ec37278ba01049ad8ba5d9de3b78b7fe3f4dc
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-20 23:47:59 -0500

    Se agrego la importacion HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
 import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, InicioComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
