bd8e5b629c73aec53a071e3ece0d82aa10fed38d
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-20 23:45:53 -0500

    Se agrego la logica para presentar la lista de los usuarios existentes

--- src/app/pages/usuario-inicio/usuario-inicio.component.ts
+++ src/app/pages/usuario-inicio/usuario-inicio.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { UsuarioService } from '../../../services/usuario.service';
+import { IUsuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-usuario-inicio',
@@ -12,5 +14,34 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './usuario-inicio.component.scss'
 })
 export class UsuarioInicioComponent {
-  displayedColumns: string[] = ['id', 'codigo', 'nombre_completo', 'correo_electronico', 'clave', 'estado', 'fecha_creacion', 'accion'];
+  private usuarioServicio = inject(UsuarioService);
+  public listaUsuario: IUsuario[]=[];
+  public displayedColumns: string[] = ['id', 'codigo', 'nombre_Completo', 'correo_Electronico', 'clave', 'estado', 'fecha_Creacion', 'accion'];
+
+  obtenerUsuario(){
+    this.usuarioServicio.lista().subscribe({
+      next:(data)=>{
+        if(data.length > 0){
+          console.log('Usuario obtenido: ', data);
+          this.listaUsuario = data;
+        }
+      },
+      error:(err)=>{
+        console.log(err.message);
+      }
+    })
+  }
+
+  constructor(private router:Router){
+    this.obtenerUsuario();
+  }
+
+  getEstado(estado: boolean): string{
+    return estado ? 'Activo': 'No Activo';
+  }
+
+  getFechaCreacion(fecha: string): string{
+    const fechaObj = new Date(fecha);
+    return fechaObj.toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' });
+  }
 }
