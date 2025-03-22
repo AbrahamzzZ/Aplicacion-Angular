6bd70c8f677a543558ec0f2b68db4d5b8444dfa6
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-22 01:18:43 -0500

    Se cambiaron algunas rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,11 @@ import { RegistroClienteComponent } from './pages/cliente-inicio/registro-client
 import { RegistroUsuarioComponent } from './pages/usuario-inicio/registro-usuario/registro-usuario.component';
 import { RegistroTransportistaComponent } from './pages/transportista-inicio/registro-transportista/registro-transportista.component';
 import { ProveedorComponent } from './pages/proveedor-inicio/registro-proveedor/registro-proveedor.component';
+import { ProductoEditarComponent } from './pages/producto-inicio/producto-editar/producto-editar.component';
+import { EditarProveedorComponent } from './pages/proveedor-inicio/editar-proveedor/editar-proveedor.component';
+import { EditarTransportistaComponent } from './pages/transportista-inicio/editar-transportista/editar-transportista.component';
+import { EditarUsuarioComponent } from './pages/usuario-inicio/editar-usuario/editar-usuario.component';
+import { EditarClienteComponent } from './pages/cliente-inicio/editar-cliente/editar-cliente.component';
 
 export const routes: Routes = [
     /*{path: 'home', component: InicioComponent, title: 'Supermercado Paradisia'},
@@ -33,15 +38,20 @@ export const routes: Routes = [
     {path: '**', component: PaginaNoEncontradaComponent}*/
     {path: 'home', component: InicioComponent, title: 'Supermercado Paradisia'},
     {path: 'usuario', component: UsuarioInicioComponent, title: 'Usuarios'},
-    {path: 'usuario/usuario-registro', component: RegistroUsuarioComponent, title: 'Información del Usuario'},
+    {path: 'usuario/usuario-registro/:id', component: RegistroUsuarioComponent, title: 'Información del Usuario'},
+    {path: 'usuario/usuario-editar/:id', component: EditarUsuarioComponent, title: 'Editar usuario'},
     {path: 'cliente', component: ClienteInicioComponent, title: 'Clientes'},
-    {path: 'cliente/cliente-registro', component: RegistroClienteComponent, title: 'Información del Cliente'},
+    {path: 'cliente/cliente-registro/:id', component: RegistroClienteComponent, title: 'Información del Cliente'},
+    {path: 'cliente/cliente-editar/:id', component: EditarClienteComponent, title: 'Editar cliente'},
     {path: 'transportista', component: TransportistaInicioComponent, title: 'Transportistas'},
-    {path: 'transportista/transportista-registro', component: RegistroTransportistaComponent, title: 'Información del Transportista'},
+    {path: 'transportista/transportista-registro/:id', component: RegistroTransportistaComponent, title: 'Información del Transportista'},
+    {path: 'transportista/transportista-editar/:id', component: EditarTransportistaComponent, title: 'Editar transportista'},
     {path: 'proveedor', component: RegistroProveedorInicioComponent, title: 'Proveedor'},
-    {path: 'proveedor/proveedor-registro', component: ProveedorComponent, title: 'Información del Proveedor'},
+    {path: 'proveedor/proveedor-registro/:id', component: ProveedorComponent, title: 'Información del Proveedor'},
+    {path: 'proveedor/proveedor-editar/:id', component:EditarProveedorComponent, title: 'Editar proveedor'},
     {path: 'producto', component: RegistroProductoInicioComponent, title: 'Productos'},
-    {path: 'producto/producto-registro', component: ProductoComponent, title: 'Información del Producto'},
+    {path: 'producto/producto-registro/:id', component: ProductoComponent, title: 'Información del Producto'},
+    {path: 'producto/producto-editar/:id', component: ProductoEditarComponent, title: 'Editar producto'},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', component: PaginaNoEncontradaComponent, title: 'Pagina no existente'}
 ];
