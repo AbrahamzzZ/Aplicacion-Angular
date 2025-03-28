750429a06fb8cdc3f5d1f226533b64a65bffe08e
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-27 23:49:14 -0500

    Se esta modificando el servicio del modulo transportista

--- src/services/transportista.service.ts
+++ src/services/transportista.service.ts
@@ -1,9 +1,8 @@
-import { HttpClient, HttpErrorResponse, HttpEventType, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { appsettings } from '../setting/appsettings';
 import { ITransportista } from '../app/models/transportista';
 import { IApi } from '../app/models/api';
-import { filter, map, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,32 +12,23 @@ export class TransportistaService {
   private apiUrl: string = appsettings.apiUrl+"Transportista";
   constructor() { }
 
-  lista(){
-      return this.http.get<ITransportista[]>(this.apiUrl);
-  }
-  
-  obtener(id:number){
-      return this.http.get<ITransportista>(`${this.apiUrl}/${id}`);
-  }
-  
-  /*registrar(transportista:ITransportista){
-      return this.http.post<IApi>(this.apiUrl, transportista);
-  }*/
-      registrar(data: FormData): Observable<IApi> {
-        return this.http.post<FormData>(this.apiUrl, data, {
-            reportProgress: true,
-            observe: 'events'
-        }).pipe(
-            filter((event: { type: HttpEventType; }): event is HttpResponse<IApi> => event.type === HttpEventType.Response),
-            map((event: HttpResponse<IApi>) => event.body!)
-        );
+    lista(){
+        return this.http.get<ITransportista[]>(this.apiUrl);
     }
   
-  editar(transportista:ITransportista){
-      return this.http.put<IApi>(this.apiUrl, transportista);
-  }
+    obtener(id:number){
+        return this.http.get<ITransportista>(`${this.apiUrl}/${id}`);
+    }
   
-  eliminar(id: number){
-      return this.http.delete<IApi>(`${this.apiUrl}/${id}`);
-  }
+    registrar(transportista:ITransportista) {
+        return this.http.post<IApi>(this.apiUrl, transportista);
+    }
+  
+    editar(transportista:ITransportista){
+        return this.http.put<IApi>(this.apiUrl, transportista);
+    }
+  
+    eliminar(id: number){
+        return this.http.delete<IApi>(`${this.apiUrl}/${id}`);
+    }
 }
