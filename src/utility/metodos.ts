a340315dbfae9459b7d8b82ed146f5b35dd473a0
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-03-31 22:59:33 -0500

    Se agrego un metodo para generar un excel

--- src/utility/metodos.ts
+++ src/utility/metodos.ts
@@ -1,3 +1,6 @@
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+
 export class Metodos {
   static generarCodigo(): string {
     return Math.floor(10000 + Math.random() * 90000).toString();
@@ -6,5 +9,16 @@ export class Metodos {
   static getFechaCreacion(): string {
     const fechaObj = new Date().toISOString();
     return fechaObj;
+  }
+
+  static exportarExcel(nombreArchivo: string, datos: any[], columnas: string[]) {
+    const ws = XLSX.utils.json_to_sheet(datos, { header: columnas });
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Productos');
+    
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const data = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+    saveAs(data, `${nombreArchivo}.xlsx`);
   }
 }
