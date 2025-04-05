2a191f75783f03c0aecf6ab97ad8957b478f8d8a
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-04-04 23:59:42 -0500

    Se agrego el servico del modulo Rol

---
+++ src/services/rol.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { appsettings } from "../setting/appsettings";
+import { IRol } from "../app/models/rol";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RolService{
+    private http = inject(HttpClient);
+    private apiUrl: string = appsettings.apiUrl + 'Rol';
+    constructor() {}
+    
+    lista() {
+        return this.http.get<IRol[]>(this.apiUrl);
+    }
+}
