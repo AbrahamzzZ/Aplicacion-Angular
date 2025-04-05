4551f4b4efff8ff66b9648b3682267a4f65c8221
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-04-04 23:55:46 -0500

    Se modifico la interfaz de Usuario

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,9 +1,12 @@
+import { IRol } from "./rol";
+
 export interface IUsuario {
   id: number;
   codigo: string;
   nombre_Completo: string;
   correo_Electronico: string;
   clave: string;
+  oRol: IRol;
   estado: boolean;
   fecha_Creacion: string;
 }
