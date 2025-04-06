260a7f7d965b79de77befc72ffa6b9055ff26066
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-04-05 23:11:21 -0500

    Se agrego la logica del boton de cerrar sesion

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { LoginService } from '../../../services/login.service';
+import jwt_decode from 'jwt-decode';
+import { IToken } from '../../models/token';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -10,13 +13,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public fechaActual: string = '';
   public horaActual: string = '';
+  public nombreUsuario: string = ''; 
+  private loginServicio = inject(LoginService);
 
-  constructor() {
+  constructor(private router: Router) {
     this.actualizarFechaHora();
     setInterval(() => this.actualizarFechaHora(), 1000); // Actualiza la hora cada segundo
+  }
+
+  ngOnInit(): void {
+    this.obtenerNombreUsuario();
   }
 
   private actualizarFechaHora(): void {
@@ -32,5 +41,24 @@ export class HeaderComponent {
       minute: '2-digit',
       second: '2-digit'
     });
+  }
+
+  private obtenerNombreUsuario(): void {
+    const token = this.loginServicio.obtenerToken();
+    if (token) {
+      try {
+        // Decodificar el token y obtener el nombre
+        /*const decodedToken: IToken = jwt_decode(token);
+        this.nombreUsuario =  decodedToken.nombre_completo || 'Usuario';*/
+      } catch (error) {
+        console.error('Error al decodificar el token:', error);
+        this.nombreUsuario = 'Usuario';
+      }
+    }
+  }
+
+  cerrarSesion(){
+    this.loginServicio.eliminarToken();
+    this.router.navigate(['/login']);
   }
 }
