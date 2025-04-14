6e58ebcb44ebb462546c6b0465198060a1e844d6
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-04-13 23:39:34 -0500

    Se agrego el formulario, los estilos y la logica para editar una categoria

--- src/app/pages/categoria-inicio/editar-categoria/editar-categoria.component.ts
+++ src/app/pages/categoria-inicio/editar-categoria/editar-categoria.component.ts
@@ -1,12 +1,131 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoriaService } from '../../../../services/categoria.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Validaciones } from '../../../../utility/validaciones';
+import { ICategoria } from '../../../models/categoria';
+import { MatCardModule } from '@angular/material/card';
+import { MatInput } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButton } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-editar-categoria',
   standalone: true,
-  imports: [],
+  imports: [    
+    MatCardModule,
+    MatInput,
+    MatFormFieldModule,
+    MatButton,
+    MatCheckboxModule,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './editar-categoria.component.html',
   styleUrl: './editar-categoria.component.scss'
 })
-export class EditarCategoriaComponent {
+export class EditarCategoriaComponent implements OnInit{
+  private categoriaServicio = inject(CategoriaService);
+  private activatedRoute = inject(ActivatedRoute);
+  private snackBar = inject(MatSnackBar);
+  private formBuilder = inject(FormBuilder);
+  idCategoria!: number;
 
+  formCategoria = this.formBuilder.nonNullable.group({
+    nombre: [
+      '',
+      [Validators.required, Validaciones.soloLetras(), Validators.maxLength(70)]
+    ],
+    estado: [false]
+  });
+
+  @HostListener('window:beforeunload', ['$event'])
+  onBeforeReload(e: BeforeUnloadEvent) {
+    const camposEditables = ['nombre'];
+    const camposConDatos = camposEditables.some(
+      (campo) => this.formCategoria.get(campo)?.value !== ''
+    );
+
+    if (camposConDatos) {
+      e.preventDefault();
+      e.returnValue = ''; // Esto es necesario para mostrar el mensaje de confirmación en algunos navegadores.
+    }
+  }
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.idCategoria = +params['id'];
+      if (this.idCategoria) {
+        this.cargarCategoria();
+      }
+    });
+  }
+
+  cargarCategoria(): void {
+    this.categoriaServicio.obtener(this.idCategoria).subscribe({
+      next: (data) => {
+        if (data) {
+          this.formCategoria.patchValue({
+            nombre: data.nombre,
+            estado: data.estado
+          });
+        }
+      },
+      error: (err) => {
+        console.error('Error al obtener categoría:', err);
+      }
+    });
+  }
+
+  editarCategoria(): void {
+    const categoria: Partial<ICategoria> = {
+      id: this.idCategoria,
+      nombre: this.formCategoria.value.nombre!,
+      estado: this.formCategoria.value.estado
+    };
+
+    this.formCategoria.markAllAsTouched();
+
+    if (!this.formCategoria.valid) {
+      this.mostrarMensaje('Formulatio inválido', 'error');
+      return;
+    }
+
+    this.categoriaServicio.editar(categoria).subscribe({
+      next: (data) => {
+        if (data.isSuccess) {
+          this.router.navigate(['/categoria']);
+          this.mostrarMensaje('¡Categoría editada exitosamente!', 'success');
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.mostrarMensaje('Error al editar la Categoría', 'error');
+      }
+    });
+  }
+
+  regresar() {
+    this.router.navigate(['/categoria']);
+  }
+
+  mostrarMensaje(mensaje: string, tipo: 'success' | 'error' = 'success') {
+    const className = tipo === 'success' ? 'success-snackbar' : 'error-snackbar';
+    
+    this.snackBar.open(mensaje, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'end',
+      verticalPosition: 'bottom',
+      panelClass: [className]
+    });
+  }
+
+  get nombreField(): FormControl<string> {
+    return this.formCategoria.controls.nombre;
+  }
 }
