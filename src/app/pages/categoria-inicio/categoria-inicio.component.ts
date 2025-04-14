09f55b69f4183f00c31ba485b482fd558708eead
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-04-13 23:41:09 -0500

    Se agrego la tabla, los estilos y la logica para presentar todas las categorias

--- src/app/pages/categoria-inicio/categoria-inicio.component.ts
+++ src/app/pages/categoria-inicio/categoria-inicio.component.ts
@@ -1,12 +1,141 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Router, RouterOutlet } from '@angular/router';
+import { CategoriaService } from '../../../services/categoria.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ICategoria } from '../../models/categoria';
+import { MatDialog } from '@angular/material/dialog';
+import { Metodos } from '../../../utility/metodos';
+import { DialogoConfirmacionComponent } from '../../components/dialogo-confirmacion/dialogo-confirmacion.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-categoria-inicio',
   standalone: true,
-  imports: [],
+  imports: [ MatTableModule,
+      MatButtonModule,
+      MatIcon,
+      MatFormFieldModule,
+      MatInputModule,
+      RouterOutlet,
+      NgClass],
   templateUrl: './categoria-inicio.component.html',
   styleUrl: './categoria-inicio.component.scss'
 })
 export class CategoriaInicioComponent {
+  private categoriaServicio = inject(CategoriaService);
+  private snackBar = inject(MatSnackBar);
+  public listaCategoria = new MatTableDataSource<ICategoria>();
+displayedColumns: string[] = [
+    'id',
+    'codigo',
+    'nombre',
+    'estado',
+    'fecha_Registro',
+    'accion'
+  ];
 
+  constructor(
+    private router: Router,
+    private dialog: MatDialog
+  ) {
+    this.obtenerCategoria();
+  }
+
+  obtenerCategoria() {
+    this.categoriaServicio.lista().subscribe({
+      next: (data) => {
+        this.listaCategoria.data = data;
+      },
+      error: (err) => {
+        console.log(err.message);
+      }
+    });
+  }
+
+  eliminar(categoria: ICategoria) {
+    const dialogRef = this.dialog.open(DialogoConfirmacionComponent, {
+      width: '350px',
+      data: {
+        mensaje: `¿Está seguro de eliminar la categoría ${categoria.nombre}?`
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.categoriaServicio.eliminar(categoria.id).subscribe({
+          next: (data) => {
+            if (data.isSuccess) {
+              this.obtenerCategoria();
+              this.mostrarMensaje('Categoría eliminado correctamente.', 'success');
+            }
+          },
+          error: (err) => {
+            console.log(err.message);
+            this.mostrarMensaje('Error al eliminar la Categoría.', 'error');
+          }
+        });
+      }
+    });
+  }
+
+  nuevo() {
+    this.router.navigate(['categoria/categoria-registro', 0]);
+  }
+
+  editar(categoria: ICategoria) {
+    this.router.navigate(['categoria/categoria-editar', categoria.id]);
+  }
+
+  mostrarMensaje(mensaje: string, tipo: 'success' | 'error' = 'success') {
+    const className = tipo === 'success' ? 'success-snackbar' : 'error-snackbar';
+    
+    this.snackBar.open(mensaje, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'end',
+      verticalPosition: 'bottom',
+      panelClass: [className]
+    });
+  }
+
+  filtrarCategorias(termino: string) {
+    this.listaCategoria.filter = termino.trim().toLowerCase();
+  }
+
+  exportarExcel() {
+    const datos = this.listaCategoria.data.map(categoria => ({
+      ID: categoria.id,
+      Código: categoria.codigo,
+      Nombre: categoria.nombre,
+      Estado: categoria.estado,
+      'Fecha Registro': this.getFechaRegistro(categoria.fecha_Registro)
+    }));
+
+    if (!datos || datos.length === 0) {
+      this.mostrarMensaje("No hay datos disponibles para exportar a Excel.", "error");
+      return;
+    }
+  
+    Metodos.exportarExcel('Productos', datos, [
+      'ID', 'Código', 'Nombre', 'Estado', 'Fecha Registro'
+    ]);
+    this.mostrarMensaje("Excel generado exitosamente.", "success");
+  }
+
+  getEstado(estado: boolean): string {
+    return estado ? 'Activo' : 'No Activo';
+  }
+
+  getFechaRegistro(fecha: string): string {
+    const fechaObj = new Date(fecha);
+    return fechaObj.toLocaleDateString('es-ES', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
+  }
 }
