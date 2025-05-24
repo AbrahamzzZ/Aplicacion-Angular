0c572e940ed51f03ee06e0e17dde673d1ec7b725
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-05-24 16:51:59 -0500

    Se creo esa carpeta para poner los modales, dialogos y layouts

---
+++ src/app/components/modal/modal-cliente/modal-cliente.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ICliente } from '../../../models/cliente';
+import { ClienteService } from '../../../../services/cliente.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-modal-cliente',
+  standalone: true,
+  imports: [MatTableModule, MatIcon],
+  templateUrl: './modal-cliente.component.html',
+  styleUrl: './modal-cliente.component.scss'
+})
+export class ModalClienteComponent {
+  listaClientes: ICliente[] = [];
+  columnas: string[] = ['id', 'nombres', 'apellidos', 'cedula', 'accion'];
+
+  constructor(
+    private dialogRef: MatDialogRef<ModalClienteComponent>,
+    private clienteService: ClienteService
+  ) {
+    this.obtenerClientes();
+  }
+
+  obtenerClientes() {
+    this.clienteService.lista().subscribe({
+      next: (data) => this.listaClientes = data,
+      error: (e) => console.error(e)
+    });
+  }
+
+  seleccionarCliente(cliente: ICliente) {
+    this.dialogRef.close(cliente);
+  }
+
+  cerrar() {
+    this.dialogRef.close();
+  }
+}
---
+++ src/app/components/modal/modal-producto/modal-producto.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { IProducto } from '../../../models/producto';
+import { ProductoService } from '../../../../services/producto.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatIcon } from '@angular/material/icon';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-modal-producto',
+  standalone: true,
+  imports: [MatTableModule, MatIcon, NgClass],
+  templateUrl: './modal-producto.component.html',
+  styleUrl: './modal-producto.component.scss'
+})
+export class ModalProductoComponent {
+  listaProductos: IProducto[] = [];
+  columnas: string[] = ['id', 'codigo','nombre', 'stock', 'estado', 'accion'];
+
+  constructor(
+    private dialogRef: MatDialogRef<ModalProductoComponent>,
+    private productoService: ProductoService
+  ) {
+    this.obtenerProductos();
+  }
+
+  obtenerProductos() {
+    this.productoService.lista().subscribe({
+      next: (data) => this.listaProductos = data,
+      error: (e) => console.error(e)
+    });
+  }
+
+  seleccionarProducto(producto: IProducto) {
+    this.dialogRef.close(producto);
+  }
+
+  cerrar() {
+    this.dialogRef.close();
+  }
+
+  getEstado(estado: boolean): string {
+    return estado ? 'Agotado' : 'No Agotado';
+  }
+}
