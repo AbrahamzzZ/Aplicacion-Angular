5322671d366aa1b69ff29cc371e2b6ae44936518
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-05-31 23:29:52 -0500

    Se creo el componente para el Modulo Sucursal

---
+++ src/app/pages/sucursal-inicio/sucursal-inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sucursal-inicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './sucursal-inicio.component.html',
+  styleUrl: './sucursal-inicio.component.scss'
+})
+export class SucursalInicioComponent {
+
+}
