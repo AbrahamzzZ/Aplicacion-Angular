ca4a988c7549a6307ec8c5033b324d6546af078a
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-06-04 22:13:22 -0500

    Se crearon los componentes para registrar y editar una sucursal

---
+++ src/app/pages/sucursal-inicio/editar-sucursal/editar-sucursal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-sucursal',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-sucursal.component.html',
+  styleUrl: './editar-sucursal.component.scss'
+})
+export class EditarSucursalComponent {
+
+}
