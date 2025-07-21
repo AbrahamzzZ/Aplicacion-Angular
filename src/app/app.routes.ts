a07a7ec9621f1c6183e463bfeee8807071b0178f
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-07-20 23:38:50 -0500

    Se dividieron ls rutas por modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -36,6 +36,7 @@ import { NegocioInicioComponent } from './pages/negocio-inicio/negocio-inicio.co
 import { SucursalInicioComponent } from './pages/sucursal-inicio/sucursal-inicio.component';
 import { RegistrarSucursalComponent } from './pages/sucursal-inicio/registrar-sucursal/registrar-sucursal.component';
 import { EditarSucursalComponent } from './pages/sucursal-inicio/editar-sucursal/editar-sucursal.component';
+import { EstadisticaNegocioComponent } from './pages/negocio-inicio/estadistica-negocio/estadistica-negocio.component';
 
 export const routes: Routes = [
   {
@@ -51,35 +52,58 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       { path: 'home', component: InicioComponent, canActivate: [Autenticacion], title: 'Inicio' },
+
+      //Modulo Usuario
       { path: 'usuario', component: UsuarioInicioComponent, canMatch: [RolGuard],canActivate: [Autenticacion], title: 'Usuarios' },
       { path: 'usuario/usuario-registro/:id', canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], component: RegistroUsuarioComponent, title: 'Registro de Usuarios' },
       { path: 'usuario/usuario-editar/:id', canActivate: [Autenticacion], component: EditarUsuarioComponent, title: 'Editar Usuario' },
+      
+      //Modulo Cliente
       { path: 'cliente', component: ClienteInicioComponent, canMatch: [RolGuard], canActivate: [Autenticacion], title: 'Clientes'},
       { path: 'cliente/cliente-registro/:id', canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], component: RegistroClienteComponent, title: 'Registro de Clientes' },
       { path: 'cliente/cliente-editar/:id', canActivate: [Autenticacion], component: EditarClienteComponent, title: 'Editar Cliente' },
+      
+      //Modulo Transportista
       { path: 'transportista', component: TransportistaInicioComponent, canMatch: [RolGuard], canActivate: [Autenticacion], title: 'Transportistas' },
       { path: 'transportista/transportista-registro/:id', canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], component: RegistroTransportistaComponent, title: 'Registro de Transportistas' },
       { path: 'transportista/transportista-editar/:id', canActivate: [Autenticacion], component: EditarTransportistaComponent, title: 'Editar Transportista' },
+      
+      //Modulo Proveedor
       { path: 'proveedor', component: ProveedorInicioComponent, canMatch: [RolGuard], canActivate: [Autenticacion], title: 'Proveedor' },
       { path: 'proveedor/proveedor-registro/:id', canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], component: RegistroProveedorComponent, title: 'Registro de Proveedores' },
       { path: 'proveedor/proveedor-editar/:id', canActivate: [Autenticacion], component: EditarProveedorComponent, title: 'Editar Proveedor' },
+      
+      //Modulo Categoría
       { path: 'categoria', component: CategoriaInicioComponent, canActivate: [Autenticacion], title: 'Categoria'},
       { path: 'categoria/categoria-registro/:id', canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], component: RegistroCategoriaComponent, title: 'Registro de Categorías'},
       { path: 'categoria/categoria-editar/:id', canActivate: [Autenticacion], component: EditarCategoriaComponent, title: 'Editar Categoría'},
+      
+      //Modulo Producto
       { path: 'producto', component: ProductoInicioComponent, canActivate: [Autenticacion], title: 'Producto' },
       { path: 'producto/producto-registro/:id', canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], component: RegistroProductoComponent, title: 'Registro de Productos'},
       { path: 'producto/producto-editar/:id', canActivate: [Autenticacion], component: ProductoEditarComponent, title: 'Editar Producto' },
+      
+      //Modulo Oferta
       { path: 'oferta', component: OfertaInicioComponent, canActivate: [Autenticacion], title: 'Oferta' },
       { path: 'oferta/oferta-registro/:id', canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], component: RegistroOfertaComponent, title: 'Registro de Ofertas' },
       { path: 'oferta/oferta-editar/:id', canActivate: [Autenticacion], component: EditarOfertaComponent, title: 'Editar Oferta' },
+      
+      //Modulo Compra
       { path: 'compra', component: CompraInicioComponent, canActivate: [Autenticacion], title: 'Registrar Compras'},
       { path: 'compra/detalle-compra',canActivate: [Autenticacion], component: DetalleCompraComponent, title: 'Ver el detalle de la compra'},
+      
+      //Modulo Venta
       { path: 'venta', component: VentaInicioComponent, canActivate: [Autenticacion], title: 'Registrar Ventas'},
       { path: 'venta/detalle-venta', canActivate: [Autenticacion], component: DetalleVentaComponent, title: 'Ver el detalle de la venta'},
+      
+      //Modulo Sucursal
       { path: 'sucursal', component: SucursalInicioComponent, canMatch: [RolGuard], canActivate: [Autenticacion], title: 'Sucursales nacionales'},
       { path: 'sucursal/sucursal-registro/:id', component: RegistrarSucursalComponent, canActivate: [Autenticacion], canDeactivate: [FormularioIncompleto], title: 'Registar Sucursal'},
       { path: 'sucursal/sucursal-editar/:id', component: EditarSucursalComponent, canActivate: [Autenticacion],title: 'Editar Sucursal'},
-      { path: 'negocio/1', component: NegocioInicioComponent, canMatch: [RolGuard], canActivate: [Autenticacion], title: 'Información del negocio'}
+      
+      //Modulo Negocio
+      { path: 'negocio/1', component: NegocioInicioComponent, canMatch: [RolGuard], canActivate: [Autenticacion], title: 'Información del negocio'},
+      { path: 'negocio/1/estadistica', component: EstadisticaNegocioComponent, canMatch: [RolGuard], canActivate: [Autenticacion], title: 'Estadisticas del negocio'}
     ]
   },
   { path: '**', component: PaginaNoEncontradaComponent, title: 'Página no encontrada' }
