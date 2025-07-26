4b696cd91c7ed20ea7e579abc7028c8424d79d6a
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-07-26 13:07:13 -0500

    Se esta implementando un buscador. Fase de prueba

--- src/app/components/modal/modal-producto/modal-producto.component.ts
+++ src/app/components/modal/modal-producto/modal-producto.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import { IProducto } from '../../../interfaces/producto';
 import { ProductoService } from '../../../../services/producto.service';
 import { MatDialogRef } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIcon } from '@angular/material/icon';
 import { NgClass } from '@angular/common';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-modal-producto',
   standalone: true,
-  imports: [MatTableModule, MatIcon, NgClass],
+  imports: [MatTableModule, MatIcon, NgClass/*, MatFormFieldModule, MatLabel, MatInputModule*/],
   templateUrl: './modal-producto.component.html',
   styleUrl: './modal-producto.component.scss'
 })
 export class ModalProductoComponent {
   listaProductos: IProducto[] = [];
+  //public listaProducto = new MatTableDataSource<IProducto>();
   columnas: string[] = ['id', 'codigo','nombre', 'stock', 'estado', 'accion'];
 
   constructor(
@@ -42,4 +45,11 @@ export class ModalProductoComponent {
   getEstado(estado: boolean): string {
     return estado ? 'Agotado' : 'No Agotado';
   }
+
+  /*filtrarProductos(termino: string) {
+    this.listaProducto.filter = termino.trim().toLowerCase();
+    if (this.listaProducto.paginator) {
+      this.listaProducto.paginator.firstPage();
+    }
+  }*/
 }
